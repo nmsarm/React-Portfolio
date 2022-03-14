c6112ad7f00fdc53887e47095d848b3e03119f49
nmsarm <daneah.sarmiento@yahoo.com>
2022-03-14 21:33:49 +0800

    NavBar Hover Underline Animation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,28 +1,31 @@
 import { Navbar, Nav } from "react-bootstrap";
 
+// Stylesheet
+import '../styles/NavBar.css'
+
 const NavBar = () => {
     return (
         <>
-        {/* comment out for transparency: bg="dark" variant="dark" */}
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="sticky-top"> 
-            <div className="container border">
+        {/* remove for transparency: bg="dark" variant="light" */}
+        <Navbar collapseOnSelect expand="lg" className="fixed-top navbar-dark"> 
+            <div className="container">
                 <Navbar.Brand href="/">
                    LOGO
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-                <Navbar.Collapse id="responsive-navbar-nav">
+                <Navbar.Collapse id="responsive-navbar-nav navbar-nav">
                     <Nav className="me-auto offset-lg-3">
                         <Nav.Link href="#home" className="offset-lg-2">
-                            <p className="pt-3"> Home </p>
+                            <p className="text-white pt-3 hover-underline-animation"> Home </p>
                         </Nav.Link>
-                        <Nav.Link href="#about" className="offset-lg-2">
-                            <p className="pt-3"> About </p>
+                        <Nav.Link href="#about" className="nav-link offset-lg-2">
+                            <p className="text-white pt-3 hover-underline-animation"> About </p>
                         </Nav.Link>
-                        <Nav.Link href="#works" className="offset-lg-2">
-                            <p className="pt-3"> Works </p>
+                        <Nav.Link href="#works" className="nav-link offset-lg-2">
+                            <p className="text-white pt-3 hover-underline-animation"> Works </p>
                         </Nav.Link>
                         <Nav.Link href="#contact" className="offset-lg-10 pt-3">
-                            <button className="btn-primary rounded p-2 shadow-sm" style={{width: "120px"}}> Contact Me </button>
+                            <button className="btn-success rounded p-2 shadow-sm" style={{width: "120px"}}> Contact Me </button>
                         </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
