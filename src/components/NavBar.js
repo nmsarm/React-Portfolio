f6739c61ca66ec9c17dd9a5cb5f43e0cc35be437
nmsarm <daneah.sarmiento@yahoo.com>
2022-04-15 19:52:11 +0800

    navbar modified

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -22,8 +22,9 @@ const NavBar = () => {
         <>
         {/* remove for transparency: bg="dark" variant="light" */}
         <Navbar 
-            collapseOnSelect expand="lg" 
-            className={navbar ? "fixed-top navbar-light" : "fixed-top navbar-dark"} 
+            collapseOnSelect 
+            expand="lg" 
+            className={navbar ? "shadow fixed-top navbar-light p-0" : "fixed-top navbar-dark"} 
             bg={ navbar ? "light" : " "}
             variant={ navbar ? "light" : " "}
         > 
@@ -31,32 +32,40 @@ const NavBar = () => {
                 <Navbar.Brand href="/">
                    <b>DaneahSarmiento</b>
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                <Navbar.Toggle className="my-2" aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav navbar-nav">
-                    <Nav className="me-auto offset-lg-3">
-                        <Nav.Link href="#home" className="offset-md-1">
+                    <Nav className={navbar ? "navbar-nav ms-auto": "me-auto offset-lg-3"}>
+                        <Nav.Link href="#home" className={navbar ? "nav-link" : "nav-link offset-md-1"}>
                             <p 
-                                className={navbar ? "text-dark pt-3 hover-underline-animation2" : "text-white pt-3 hover-underline-animation"}
+                                className={navbar ? "pt-3 hover-underline-animation2" : "text-white pt-3 hover-underline-animation"}
                             > 
                                 Home 
                             </p>
                         </Nav.Link>
-                        <Nav.Link href="#about" className="nav-link offset-md-2 current">
+                        <Nav.Link href="#about"className={navbar ? "nav-link" : "nav-link offset-md-2"}>
                             <p 
-                                className={navbar ? "text-dark pt-3 hover-underline-animation2" : "text-white pt-3 hover-underline-animation"}
+                                className={navbar ? "pt-3 hover-underline-animation2" : "text-white pt-3 hover-underline-animation"}
                             > 
                                 About 
                             </p>
                         </Nav.Link>
-                        <Nav.Link href="#works" className="nav-link offset-md-2">
+                        <Nav.Link href="#works" className={navbar ? "nav-link" : "nav-link offset-md-2"}>
                             <p 
-                                className={navbar ? "text-dark pt-3 hover-underline-animation2" : "text-white pt-3 hover-underline-animation"}
+                                className={navbar ? "pt-3 hover-underline-animation2" : "text-white pt-3 hover-underline-animation"}
                             > 
                                 Works 
                             </p>
                         </Nav.Link>
-                        <Nav.Link href="#contact" className="offset-lg-10 pt-3">
-                            <button className="btn-success rounded p-2 shadow" style={{width: "120px"}}> Contact Me </button>
+                        <Nav.Link href="#contact" className={navbar ? "nav-link" : "offset-lg-10 pt-3"}>
+                            {navbar ? 
+                                 <p 
+                                 className={navbar ? "pt-3 hover-underline-animation2" : "text-white pt-3 hover-underline-animation"}
+                                    > 
+                                       Contact Me
+                                    </p>
+                                : 
+                                <button className="btn-success rounded p-2 shadow" style={{width: "120px"}}> Contact Me </button>
+                            }
                         </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
