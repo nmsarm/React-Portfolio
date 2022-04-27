cbdddbc89116b43bf04e47ce87227534b33f9f8b
nmsarm <daneah.sarmiento@yahoo.com>
2022-04-28 01:15:24 +0800

    added other works section, card template

--- src/components/SkillCard.js
+++ src/components/SkillCard.js
@@ -5,23 +5,23 @@ import { useEffect } from 'react';
 
 // Card Component - Props
 const SkillCard = (props) => {
-    
+
     useEffect(() => {
-        Aos.init({duration: 500});
+        Aos.init({ duration: 500 });
     }, []);
 
     return (
-        <>
-            <div className="skills-card"  data-aos="flip-left">
-                <div className="skills-box">
-                    <div className="skills-content">
-                        <h2>{props.num}</h2>
-                        <h3>{props.title}</h3>
-                        <p className="font-400">{props.txt}</p>
-                    </div>
+
+        <div className="skills-card" data-aos="flip-left">
+            <div className="skills-box">
+                <div className="skills-content">
+                    <h2>{props.num}</h2>
+                    <h3>{props.title}</h3>
+                    <p className="font-400">{props.txt}</p>
                 </div>
             </div>
-        </>
+        </div>
+
     );
 }
 
--- src/pages/LatestWork.js
+++ src/pages/LatestWork.js
@@ -1,3 +1,4 @@
+import React from 'react'
 // Stylesheet
 import '../styles/LatestWork.scss'
 
