6164399138bf3bf54eb712d7dcb4f67b01ce9431
nmsarm <daneah.sarmiento@yahoo.com>
2022-05-02 05:00:45 +0800

    latestwork responsive

--- src/pages/LatestWork.js
+++ src/pages/LatestWork.js
@@ -9,11 +9,11 @@ const About = () => {
         <div className="latestwork-container container-fluid">
             <div className="innerLW-container shadow-lg">
                 <div className="row mx-5 my-3 py-5">
-                    <div className="col">
+                    <div className="col-lg-6 col-sm-12">
                         <h2 className="fw-bold pb-2">Latest Work </h2>
                         <h4 className="fw-bold py-2" style={{ color: "#198754" }}>Developer Portfolio</h4>
                         <p className="latestwork-desc"> I am currently working on this website! This project started during my internship at TELUS International Philippines, Inc.</p>
-                        <div className="col-10">
+                        <div className="col-10 col-sm-10 pb-sm-4">
                             <button type="button" className="lw-btn btn btn-dark">HTML</button>
                             <button type="button" className="lw-btn btn btn-dark">SCSS</button>
                             <button type="button" className="lw-btn btn btn-dark">Bootstrap</button>
@@ -22,7 +22,7 @@ const About = () => {
                             <button type="button" className="lw-btn btn btn-dark">GitHub</button>
                         </div>
                     </div>
-                    <div className="col">
+                    <div className="col-lg-6 col-sm-12">
                         <div class="terminal-window">
                             <div class="terminal-title">
                                 <div class="terminal-title-btn">
