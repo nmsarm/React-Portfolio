b9e070d744a7a078479656631d2e9e04251276a3
nmsarm <daneahsarmiento@gmail.com>
2022-05-02 11:37:28 +0800

    navbar responsiveness

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -58,22 +58,22 @@ const NavBar = () => {
                     </Navbar.Brand>
                     <Navbar.Toggle className="my-2" aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav navbar-nav">
-                        <Nav className={navbar ? "navbar-nav ms-auto" : "me-auto offset-lg-3"}>
-                            <Nav.Link href="#home" className={navbar ? "nav-link" : "nav-link offset-md-1"}>
+                        <Nav className={navbar ? "navbar-nav ms-auto" : "me-auto offset-lg-3" && isMobile? "navbar-nav ms-auto" : "me-auto offset-3"}>
+                            <Nav.Link href="#home" className={navbar ? "nav-link" : "nav-link offset-md-1" && isMobile? "nav-link" : "nav-link offset-md-1"}>
                                 <p
                                     className={navbar ? "pt-3 hover-underline-animation2" : "text-white pt-3 hover-underline-animation"}
                                 >
                                     Home
                                 </p>
                             </Nav.Link>
-                            <Nav.Link href="#about" className={navbar ? "nav-link" : "nav-link offset-md-2"}>
+                            <Nav.Link href="#about" className={navbar ? "nav-link" : "nav-link offset-md-2" && isMobile? "nav-link" : "nav-link offset-md-2"}>
                                 <p
                                     className={navbar ? "pt-3 hover-underline-animation2" : "text-white pt-3 hover-underline-animation"}
                                 >
                                     About
                                 </p>
                             </Nav.Link>
-                            <Nav.Link href="#works" className={navbar ? "nav-link" : "nav-link offset-md-2"}>
+                            <Nav.Link href="#works" className={navbar ? "nav-link" : "nav-link offset-md-2" && isMobile? "nav-link" : "nav-link offset-md-2"}>
                                 <p
                                     className={navbar ? "pt-3 hover-underline-animation2" : "text-white pt-3 hover-underline-animation"}
                                 >
